6f4b726cd4dd3e3cae5bc5f959d9d8b33e7711f1
catx1726 <catx1726@foxmail.com>
2020-05-20 19:13:53 +0800

    尝鲜..

---
+++ vue-next-template/src/components/MyCard.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: 'MyCard',
+  props: {
+    name: { default: '', type: String },
+    person: { default: () => {}, type: Object }
+  },
+  setup(props, { emit }) {
+    const childClick = () => {
+      emit('addLike')
+    }
+    return { props, childClick }
+  }
+}
+</script>
+<template>
+  <div class="article-container">
+    <div class="title">
+      Vue3 Yes！
+    </div>
+    <div class="subtitle">author:{{ props.person.name }} | posted:2020年5月20日</div>
+    <div class="content">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam aliquam, itaque deserunt
+      voluptate reiciendis nobis voluptatibus labore autem, ex consectetur beatae quibusdam ab
+      accusantium amet voluptatem quisquam? Aliquam, excepturi earum.
+    </div>
+    <div class="information">
+      Like:{{ props.person.like }}
+      <button @click="childClick">Like?</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.article-container {
+  display: flex;
+  left: 50%;
+  top: 30%;
+  width: 60vw;
+  position: relative;
+  transform: translate(-50%, -30%);
+  flex-direction: column;
+  background-color: beige;
+  align-items: center;
+  .title {
+    font-size: 50px;
+  }
+  .subtitle {
+    font-size: 16px;
+    opacity: 0.6;
+    padding-bottom: 10px;
+  }
+  .content {
+    width: 40vw;
+    text-indent: 1cm;
+    text-align: start;
+    padding-bottom: 10px;
+  }
+}
+</style>
--- vue-next-template/src/views/About.vue
+++ vue-next-template/src/views/About.vue
@@ -1,5 +1,36 @@
+<script>
+import { reactive, ref, onMounted } from 'vue'
+import MyCard from '@/components/MyCard'
+export default {
+  components: {
+    MyCard
+  },
+  setup() {
+    const person = reactive({
+      name: 'cad',
+      age: '18',
+      like: 0
+    })
+
+    const card = ref(person)
+
+    const addLike = () => {
+      card.value.like++
+    }
+
+    onMounted(() => {
+      console.log('life hooks onMounted!')
+    })
+
+    return { person, card, addLike }
+  }
+}
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" style="height:900px">
+    <h1>Hello {{ person.name }}</h1>
+    <h1>Like:{{ card.like }}</h1>
+    <button @click="addLike">click me please</button>
+    <MyCard :person="person" @addLike="addLike" />
   </div>
 </template>
